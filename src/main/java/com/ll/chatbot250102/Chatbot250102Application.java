135c7659ac9719a5ddfee0cef1a78d32714ceba8
sik2 <bickwang123@gmail.com>
2025-01-06 05:49:51 +0900

    init

---
+++ src/main/java/com/ll/chatbot250102/Chatbot250102Application.java
@@ -0,0 +1,13 @@
+package com.ll.chatbot250102;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chatbot250102Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chatbot250102Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/chatbot250102/Chatbot250102ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.chatbot250102;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chatbot250102ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
